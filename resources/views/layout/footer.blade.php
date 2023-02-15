db4bc4a7bdbe5318a443a8ab3c4dd978223039fa
Dương Tuấn <tuanmixmusic@gmail.com>
2023-02-15 23:45:26 +0700

    setup project

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl d-flex flex-wrap justify-content-center">
+        <div class="mb-2 mb-md-0">
+            ©
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            , made with ❤️ by
+            <a href="https://github.com/tuanduongg" target="_blank" class="footer-link fw-bolder">Toilatuan</a>
+        </div>
+
+    </div>
+</footer>
